9d7a2893c1af1c089ad300e4d5a7ef4b64e8c1d6
HyoJung KIm <hyojungkim2000@gmail.com>
2014-07-27 19:43:06 +0900

    Shiny project ui.R and server.R

---
+++ server.R
@@ -0,0 +1,16 @@
+library(shiny)
+
+estimateHeight <- function(finput,minput,genderadjust) (finput+minput)/2+as.numeric(genderadjust)*6.35
+
+shinyServer(
+        function(input, output) {
+                output$fhoutput <- renderPrint({input$fhinput})
+                output$mhoutput <- renderPrint({input$mhinput})
+                output$genderout <- renderPrint({if (input$gender =='-1') paste("Girl") else paste("Boy")})
+                output$prediction <- renderPrint({estimateHeight(input$fhinput, input$mhinput, input$gender)})                
+                }
+)
+
+
+
+                
